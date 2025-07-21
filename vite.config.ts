5aef40dee9158e504d966bdf0ff4d1e2ddad6e61
Stas <stas.krivosheev.0054@gmail.com>
2025-07-21 17:57:57 +0300

    fix dist

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,12 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'PyxisUiKit',
-      fileName: (format) => `pyxis-ui-kit.${format}.js`,
+      formats: ['es', 'cjs'],
+      fileName: (format) => {
+        if (format === 'es') return 'index.js';
+        if (format === 'cjs') return 'index.cjs';
+        return `pyxis-ui-kit.${format}.js`;
+      },
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'antd', 'classnames'],
